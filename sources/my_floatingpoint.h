47c07e0d223a070459afb1f81bcbf5add3c1585e
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-07-30 00:14:11 +0200

    i

--- sources/my_floatingpoint.h
+++ sources/my_floatingpoint.h
@@ -1,7 +1,7 @@
 #ifndef FLOATING_POINT_H
 # define FLOATING_POINT_H
 
-# include "floating_point_bit_patterns.h"
+# include "my_floatingpoint_bit_patterns.h"
 
 typedef struct	s_decomposed_floating_point {
 	uint32_t	flags;
@@ -9,11 +9,6 @@ typedef struct	s_decomposed_floating_point {
 	uint64_t	mant;
 	uint64_t	aligned;
 }				t_s_dfp;
-
-typedef struct	s_fp_and_dfp {
-	t_s_dfp		dec;
-	long double	dbl;
-}				t_s_fpndfp;
 
 # define MANTISSA_LOG2_RESOLUTION 63
 # define MANT_RES MANTISSA_LOG2_RESOLUTION 
@@ -26,8 +21,9 @@ void
 	decompose_double(
 		double const *p_val, t_s_dfp *p_ret);
 
+/*implement this*/
 long double
-	compose_float(
+	compose_ldouble(
 		char sign, uint64_t times, int32_t power);
 
 #endif
