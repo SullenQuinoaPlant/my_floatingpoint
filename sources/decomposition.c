0ed70c4f507341adefda8272bd2aa37a1a2f924f
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-07-30 21:16:24 +0200

    i

--- sources/decomposition.c
+++ sources/decomposition.c
@@ -2,38 +2,36 @@
 
 void
 	decompose_double(
-		double const *val, t_s_dfp *p_ret)
+		double const *val, t_s_dfp *ret)
 {
 	t_u_d	arg;
 	
 	ft_memcpy(&arg, val, sizeof(*val));
-	*p_ret = (t_s_dfp){arg.sign, exp_bias_d(arg.exp), arg.mant};
+	*ret = (t_s_dfp){arg.sign, exp_unbias_d(arg.exp), arg.mant};
 	if (arg.exp == ~0)
-		p_ret->flags |= arg.mant ? NAN_F : INF_F;
+		ret->flags |= arg.mant ? NAN_F : INF_F;
 	else if (arg.exp == 0)
 	{
-		p_ret->flags |= DNORM_F; 
-		p_ret->exp += arg.mant ? 1 : DB_E_BIAS;
+		ret->flags |= DNORM_F; 
+		ret->exp = arg.mant ? ret->exp : 0;
 	}
 	else
-		p_ret->mant |= 1ull<<52;
+		ret->mant |= 1ull << DB_MANT_BITS;
 }
 
 void
 	decompose_ldouble(
-		long double const *val, t_s_dfp *p_ret)
+		long double const *val, t_s_dfp *ret)
 {
 	t_u_ld	arg;
 	
 	ft_memcpy(&arg, val, sizeof(*val));
-	*p_ret = (t_s_dfp){arg.sign, arg.exp - LDB_E_BIAS,
-						arg.mant, arg.mant << 1};
+	*ret = (t_s_dfp){arg.sign, exp_unbias_ld(arg.exp), arg.mant};
 	if (arg.exp == ~0)
-		p_ret->flags |= arg.mant ? NAN_F : INF_F;
+		ret->flags |= arg.mant ? NAN_F : INF_F;
 	else if (arg.exp == 0)
 	{
-		p_ret->flags |= DNORM_F;
-		p_ret->exp += arg.mant ? 1 : LDB_E_BIAS;
+		ret->flags |= DNORM_F;
+		ret->exp += arg.mant ? 0 : -ret->exp;
 	}
 }
-#endif
