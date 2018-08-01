56bafbc52c4a5eaddeeb3b3160405a89d1a65ab0
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-08-01 18:21:36 +0200

    i

--- sources/make_ldouble.c
+++ sources/make_ldouble.c
@@ -33,7 +33,9 @@ long double
 	make_ldouble(
 		char s, uint64_t m, int32_t p)
 {
-	if (align_exp(&m, &p))
+	if (!m)
+		return (0.0L);
+	else if (align_exp(&m, &p))
 		return (compose_ldouble(s, m, exp_bias_ld(p)));
 	else
 		return (compose_inf(s));
