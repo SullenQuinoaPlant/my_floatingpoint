ccf5658dc335c5bd6e28d8651df4f85c4a8cbfd7
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-08-01 19:42:04 +0200

    i

---
+++ sources/make_double.c
@@ -0,0 +1,44 @@
+#include "inner.h"
+
+static int
+	is_too_much(
+		int32_t pow)
+{
+	if (!(pow <= DB_MAX_POW && pow >= DB_MIN_POW))
+		return (1);
+	return (0);
+}
+
+static int
+	align_exp(
+		uint64_t *times, int32_t *pow)
+{
+	uint64_t	mask;
+
+	mask = ~((uint64_t)0) << DB_MANT_PRECISION;
+	while (*pow > DB_MIN_POW && !(*times & mask))
+	{
+		*times <<= 1;
+		(*pow)--;
+	}
+	mask <<= 1;
+	while ((*pow < DB_MIN_POW && *times ||
+		(*pow >= DB_MIN_POW && *times & mask))
+	{
+		*times >>= 1;
+		(*pow)++;
+	}
+	return (!is_too_much(*pow));
+}
+
+long double
+	make_ldouble(
+		char s, uint64_t m, int32_t p)
+{
+	if (!m)
+		return (0.0);
+	else if (align_exp(&m, &p))
+		return (compose_ldouble(s, m, exp_bias_ld(p)));
+	else
+		return (compose_inf(s));
+}
