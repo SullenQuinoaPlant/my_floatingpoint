ac655dea6e4815b032cbb626389ead5de960050a
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-08-05 02:39:15 +0200

    i

--- sources/floorl.c
+++ sources/floorl.c
@@ -8,7 +8,7 @@ long double
 	int32_t		pow;
 
 	decompose_ldouble(&d, &dec);
-	if ((pow = dec.exp - LDM_MANT_PRECISION) >= 0)
+	if ((pow = dec.exp - LDB_MANT_PRECISION) >= 0)
 		return (d);
 	dec.mant &= ~((uint64_t)0) << -pow;
 	if ((d = make_ldouble(d < 0, dec.mant, pow)) <= 0)
