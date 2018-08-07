b3073bce8acf5ccd74189611c29363381a41a36c
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-08-07 05:21:55 +0200

    i

--- unit-tests/floorl.test.c
+++ unit-tests/floorl.test.c
@@ -7,10 +7,10 @@
 int	declare_tests_and_run(int all_of, char *these[])
 {
 	T(test1,
-		assert_true(floorl(0.1) == 0);
+		assert_true(my_floorl(0.1) == 0);
 	)
 	T(test2,
-		assert_true(floorl(-0.1) == -1);
+		assert_true(my_floorl(-0.1) == -1);
 	)
 	return(run_test_arr(all_of, these));
 }
