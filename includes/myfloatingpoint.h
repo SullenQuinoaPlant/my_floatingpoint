1cbffc086e2a94610cc0638c0684b2292c3c0adf
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-14 13:34:43 +0200

    i

--- includes/myfloatingpoint.h
+++ includes/myfloatingpoint.h
@@ -6,32 +6,36 @@
 /*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/14 11:27:55 by nmauvari          #+#    #+#             */
-/*   Updated: 2018/09/14 11:27:57 by nmauvari         ###   ########.fr       */
+/*   Updated: 2018/09/14 13:34:16 by nmauvari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FLOATING_POINT_H
-# define FLOATING_POINT_H
+#ifndef MYFLOATINGPOINT_H
+# define MYFLOATINGPOINT_H
 
 # include <stdint.h>
 
-/*t_s_dfp flags*/
+/*
+**t_s_dfp flags
+*/
 # define SIGN_F 0x01
 # define INF_F 0x02
 # define NAN_F 0x04
 # define OVER_F 0x08
 # define DNORM_F 0x10
 
-typedef struct	s_decomposed_floating_point {
+typedef struct				s_decomposed_floating_point
+{
 	uint32_t	flags;
 	int32_t		exp;
 	uint64_t	mant;
-}				t_s_dfp;
+}							t_s_dfp;
 
-typedef struct	s_fp_and_dfp {
+typedef struct				s_fp_and_dfp
+{
 	long double	dbl;
 	t_s_dfp		dec;
-}				t_s_fpndfp;
+}							t_s_fpndfp;
 
 # define DB_MANT_BITS 52
 # define DB_EXP_BITS 11
@@ -55,86 +59,77 @@ typedef struct	s_fp_and_dfp {
 # define LDB_MAX_EXP 16383
 # define LDB_MAX_POW LDB_MAX_EXP - LDB_MANT_PRECISION
 
-/*functions*/
-double
-	compose_double(
-		char sign, uint64_t mant, uint16_t bias_exp);
+/*
+**functions
+*/
+double						compose_double(
+	char		sign,
+	uint64_t	mant,
+	uint16_t	bias_exp);
 
-double
-	compose_inf(
-		char sign);
+double						compose_inf(
+	char		sign);
 
-long double
-	compose_ldouble(
-		char sign, uint64_t times, uint16_t bias_exp);
+long double					compose_ldouble(
+	char		sign,
+	uint64_t	times,
+	uint16_t	bias_exp);
 
-double
-	compose_minf();
+double						compose_minf(void);
 
-double
-	compose_nan();
+double						compose_nan(void);
 
-double
-	compose_pinf();
+double						compose_pinf(void);
 
-void
-	decompose_ldouble(
-		long double const *p_val, t_s_dfp *p_ret);
+void						decompose_ldouble(
+	long double const	*p_val,
+	t_s_dfp				*p_ret);
 
-void
-	decompose_double(
-		double const *p_val, t_s_dfp *p_ret);
+void						decompose_double(
+	double const	*p_val,
+	t_s_dfp			*p_ret);
 
-uint16_t
-	exp_bias_d(
-		int32_t pow);
+uint16_t					exp_bias_d(
+	int32_t		pow);
 
-uint16_t
-	exp_bias_ld(
-		int32_t pow);
+uint16_t					exp_bias_ld(
+	int32_t		pow);
 
-int32_t
-	exp_unbias_d(
-		uint16_t pow);
+int32_t						exp_unbias_d(
+	uint16_t	pow);
 
-int32_t
-	exp_unbias_ld(
-		uint16_t pow);
+int32_t						exp_unbias_ld(
+	uint16_t	pow);
 
-long double
-	my_floorl(
-		long double d);
+long double					my_floorl(
+	long	double d);
 
-int
-	has_nanf(
-		t_s_dfp *dec);
+int							has_nanf(
+	t_s_dfp	*dec);
 
-int
-	is_dnorm(
-		double dbl);
+int							is_dnorm(
+	double	dbl);
 
-int
-	is_finite(
-		double dbl);
+int							is_finite(
+	double	dbl);
 
-int
-	is_inf(
-		double dbl);
+int							is_inf(
+	double	dbl);
 
-int
-	is_nan(
-		double dbl);
+int							is_nan(
+	double	dbl);
 
-int
-	is_overf(
-		double dbl);
+int							is_overf(
+	double	dbl);
 
-double
-	make_double(
-		char s, uint64_t times, int32_t p);
+double						make_double(
+	char		s,
+	uint64_t	times,
+	int32_t		p);
 
-long double
-	make_ldouble(
-		char sign, uint64_t times, int32_t pow);
+long double					make_ldouble(
+	char		sign,
+	uint64_t	times,
+	int32_t		pow);
 
 #endif
